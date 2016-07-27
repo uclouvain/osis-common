24612ae8703ca8d391419df5b7dd59c35762f4bc
Gaëtan La Marca <gaetan.lamarca@uclouvain.be>
2016-07-27 21:19:37 +0200

    Revert "Revert "Issue#8""
    This reverts commit 60b80bc0d76aae8528b39259b8c9ea9b62c43927.
    
    après rebase

--- admin.py
+++ admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from osis_common.models import message_template, message_history
+from osis_common.models import message_template, message_history, document_file
 
 admin.site.register(message_template.MessageTemplate,
                     message_template.MessageTemplateAdmin)
 admin.site.register(message_history.MessageHistory,
                     message_history.MessageHistoryAdmin)
+admin.site.register(document_file.DocumentFile,
+                    document_file.DocumentFileAdmin)
