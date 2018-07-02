9c92d27e0f33845e0127a3e26570dd775877334d
Ndizera Eddy <eddyndizera@gmail.com>
2018-07-02 11:55:27 +0200

    OSIS-1251 Create permission utilities 	new file:   tests/utils/test_perms.py 	new file:   utils/perms.py

---
+++ tests/utils/test_perms.py
@@ -0,0 +1,67 @@
+##############################################################################
+#
+#    OSIS stands for Open Student Information System. It's an application
+#    designed to manage the core business of higher education institutions,
+#    such as universities, faculties, institutes and professional schools.
+#    The core business involves the administration of students, teachers,
+#    courses, programs and so on.
+#
+#    Copyright (C) 2015-2018 Université catholique de Louvain (http://www.uclouvain.be)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU General Public License for more details.
+#
+#    A copy of this license - GNU General Public License - is available
+#    at the root of the source code of this program.  If not,
+#    see http://www.gnu.org/licenses/.
+#
+##############################################################################
+from django.core.exceptions import PermissionDenied
+from django.test import SimpleTestCase
+
+from osis_common.utils.perms import BasePerm
+
+error_msg1 = "Number too big"
+error_msg2 = "Number is not pair"
+
+def predicate1(n):
+    value = n <= 10
+    if not value:
+        raise PermissionDenied(error_msg1)
+
+
+def predicate2(n):
+    value = n % 2 == 0
+    if not value:
+        raise PermissionDenied(error_msg2)
+
+
+class IsNumberLegit(BasePerm):
+    predicates = (predicate1, predicate2)
+
+
+class TestBasePerms(SimpleTestCase):
+    def test_errors(self):
+        errors = IsNumberLegit(11).errors
+        self.assertListEqual(errors, [error_msg1, error_msg2])
+
+        errors = IsNumberLegit(9).errors
+        self.assertListEqual(errors, [error_msg2])
+
+        errors = IsNumberLegit(6).errors
+        self.assertListEqual(errors, [])
+
+    def test_is_valid(self):
+        is_valid = IsNumberLegit(9).is_valid()
+        self.assertFalse(is_valid)
+
+        is_valid = IsNumberLegit(6).is_valid()
+        self.assertTrue(is_valid)
+
---
+++ utils/perms.py
@@ -0,0 +1,81 @@
+##############################################################################
+#
+#    OSIS stands for Open Student Information System. It's an application
+#    designed to manage the core business of higher education institutions,
+#    such as universities, faculties, institutes and professional schools.
+#    The core business involves the administration of students, teachers,
+#    courses, programs and so on.
+#
+#    Copyright (C) 2015-2018 Université catholique de Louvain (http://www.uclouvain.be)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    A copy of this license - GNU General Public License - is available
+#    at the root of the source code of this program.  If not,
+#    see http://www.gnu.org/licenses/.
+#
+##############################################################################
+import sys
+
+from django.core.exceptions import PermissionDenied
+from django.utils.functional import cached_property
+
+
+class BasePerm:
+    predicates = None
+
+    def __init__(self, *args, **kwargs):
+        self.predicates_arguments = args
+
+    @cached_property
+    def errors(self):
+        predicates_errors = (self._call_predicate(p) for p in self.predicates)
+        return [e for e in predicates_errors if e]
+
+    def _call_predicate(self, predicate):
+        try:
+            predicate(*self.predicates_arguments)
+        except PermissionDenied as e:
+            return str(e)
+        return ""
+
+    def is_valid(self):
+        return not self.errors
+
+
+
+
+def conjunction(*predicates):
+
+    def conjunction_method(*args, **kwargs):
+        return all(
+            p(*args, **kwargs) for p in predicates
+        )
+
+    return conjunction_method
+
+
+def disjunction(*predicates):
+
+    def disjunction_method(*args, **kwargs):
+        return any(
+            p(*args, **kwargs) for p in predicates
+        )
+
+    return disjunction_method
+
+
+def negation(predicate):
+
+    def negation_method(*args, **kwargs):
+        return not predicate(*args, **kwargs)
+
+    return negation_method
