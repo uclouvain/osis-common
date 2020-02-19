b2d839715e58c7ae124f29bcf694c5f62365ab1b
Mathieu Zen <zen.mathieu@gmail.com>
2020-02-19 15:55:28 +0100

    fix imports + update license

--- tests/utils/test_models.py
+++ tests/utils/test_models.py
@@ -6,7 +6,7 @@
 #    The core business involves the administration of students, teachers,
 #    courses, programs and so on.
 #
-#    Copyright (C) 2015-2018 Université catholique de Louvain (http://www.uclouvain.be)
+#    Copyright (C) 2015-2020 Université catholique de Louvain (http://www.uclouvain.be)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -23,11 +23,11 @@
 #    see http://www.gnu.org/licenses/.
 #
 ##############################################################################
-from base.models.utils.utils import get_object_or_none
 from django.test import TestCase
 
 from base.models.person import Person
 from base.tests.factories.person import PersonFactory
+from osis_common.utils.models import get_object_or_none
 
 
 class UtilsTest(TestCase):
