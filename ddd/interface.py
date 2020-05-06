82dafe9f99fe96b94214a7109e4a7ffea5a1c9fa
Alessandro Brugnetti <alessandro.brugnetti@uclouvain.be>
2020-05-06 10:57:19 +0200

    Add Interface for DDD

---
+++ ddd/interface.py
@@ -0,0 +1,44 @@
+import abc
+
+
+class ValueObject(abc.ABC):
+    def __eq__(self, other):
+        raise NotImplementedError
+
+    def __hash__(self):
+        raise NotImplementedError
+
+
+class EntityIdentity(ValueObject):
+    pass
+
+
+class Entity(abc.ABC):
+    def __init__(self, *args, entity_id: EntityIdentity = None, **kwargs):
+        if entity_id is not None:
+            self.entity_id = entity_id
+        super().__init__(*args, **kwargs)
+
+    def __eq__(self, other):
+        return self.entity_id == other.entity_id
+
+    def __hash__(self):
+        raise hash(self.entity_id)
+
+
+class RootEntity(Entity):
+    pass
+
+
+class AbstractRepository(abc.ABC):
+    @classmethod
+    @abc.abstractmethod
+    def add(self, entity: Entity):
+        raise NotImplementedError
+
+    @classmethod
+    @abc.abstractmethod
+    def get(self, entity_id: EntityIdentity) -> Entity:
+        raise NotImplementedError
+
+    #  TODO Add method which are common accros repository
