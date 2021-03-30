062977b46c587aa43280470da62820a8cc291354
Bastien Mottiaux <bastien.mottiaux@uclouvain.be>
2021-03-30 14:46:41 +0200

    post workshop 5 : add AbstractRepository.save() and make create() and update() deprecated

--- ddd/interface.py
+++ ddd/interface.py
@@ -1,4 +1,5 @@
 import abc
+import warnings
 from typing import List, Optional, Callable, Union
 
 
@@ -51,41 +52,51 @@ ApplicationService = Callable[[CommandRequest], Union[EntityIdentity, List[Entit
 
 
 class AbstractRepository(abc.ABC):
+
     @classmethod
-    @abc.abstractmethod
-    def create(cls, entity: Entity, **kwargs: ApplicationService) -> EntityIdentity:
+    def create(cls, entity: Entity, **kwargs: ApplicationService) -> RootEntity:
         """
         Function used to persist (create) new domain Entity into the database.
         :param entity: Any domain Entity.
         :param services: List of application services used to persist data into another domain.
         :return: The identity of the created entity.
         """
-        raise NotImplementedError
+        warnings.warn("DEPRECATED : use .save() function instead", DeprecationWarning)
 
     @classmethod
-    @abc.abstractmethod
-    def update(cls, entity: Entity, **kwargs: ApplicationService) -> EntityIdentity:
+    def update(cls, entity: Entity, **kwargs: ApplicationService) -> RootEntity:
         """
         Function used to persist (update) existing domain Entity into the database.
         :param entity: Any domain Entity.
         :param services: List of application services used to persist data into another domain.
         :return: The identity of the updated entity.
         """
+        warnings.warn("DEPRECATED : use .save() function instead", DeprecationWarning)
+
+    @classmethod
+    @abc.abstractmethod
+    def get(cls, entity_id: EntityIdentity) -> RootEntity:
         raise NotImplementedError
 
     @classmethod
     @abc.abstractmethod
-    def get(cls, entity_id: EntityIdentity) -> Entity:
-        raise NotImplementedError
-
-    @classmethod
-    @abc.abstractmethod
-    def search(cls, entity_ids: Optional[List[EntityIdentity]] = None, **kwargs) -> List[Entity]:
+    def search(cls, entity_ids: Optional[List[EntityIdentity]] = None, **kwargs) -> List[RootEntity]:
         raise NotImplementedError
 
     @classmethod
     @abc.abstractmethod
     def delete(cls, entity_id: EntityIdentity, **kwargs: ApplicationService) -> None:
+        raise NotImplementedError
+
+    @classmethod
+    @abc.abstractmethod
+    def save(cls, entity: RootEntity) -> None:
+        """
+        Function used to persist existing domain RootEntity (aggregate) into the database.
+        :param entity: Any domain Entity.
+        :return: The identity of the updated entity.
+
+        """
         raise NotImplementedError
 
 
@@ -111,10 +122,12 @@ class DTO:
 class RootEntityBuilder(abc.ABC):
 
     @classmethod
+    @abc.abstractmethod
     def build_from_command(cls, cmd: 'CommandRequest') -> 'RootEntity':
         raise NotImplementedError()
 
     @classmethod
+    @abc.abstractmethod
     def build_from_repository_dto(cls, dto_object: 'DTO') -> 'RootEntity':
         raise NotImplementedError()
 
@@ -122,9 +135,11 @@ class RootEntityBuilder(abc.ABC):
 class EntityIdentityBuilder(abc.ABC):
 
     @classmethod
+    @abc.abstractmethod
     def build_from_command(cls, cmd: 'CommandRequest') -> 'EntityIdentity':
         raise NotImplementedError()
 
     @classmethod
+    @abc.abstractmethod
     def build_from_repository_dto(cls, dto_object: 'DTO') -> 'EntityIdentity':
         raise NotImplementedError()
