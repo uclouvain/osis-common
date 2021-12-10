da5b063d98980039a17931a5a716f13d8d2198f8
Benjamin Daubry <benjamin.daubry@uclouvain.be>
2021-12-10 12:26:22 +0100

    Fix inti=

--- ddd/interface.py
+++ ddd/interface.py
@@ -2,14 +2,14 @@ import abc
 import uuid
 import warnings
 from decimal import Decimal
-from typing import List, Optional, Callable, Union, Dict
+from typing import List, Optional, Callable, Union
 
 import attr
 
 
-@attr.s(frozen=True, slots=True)
+@attr.s(frozen=True, slots=True, auto_attribs=True)
 class CommandRequest(abc.ABC):
-    transaction_id = attr.ib(init=False, type=uuid.UUID, default=attr.Factory(uuid.uuid4), eq=False)
+    transaction_id: uuid.UUID = attr.ib(init=False, default=attr.Factory(uuid.uuid4), eq=False)
 
 
 class BusinessException(Exception):
