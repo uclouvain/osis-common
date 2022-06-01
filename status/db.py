3c16abedf017b02e6e5da7b5890e941fc5fe9fbe
Eddy Ndizera <eddy.ndizera@uclouvain.be>
2022-06-01 11:42:21 +0200

    implement api to check service status

---
+++ status/db.py
@@ -0,0 +1,44 @@
+#
+#    OSIS stands for Open Student Information System. It's an application
+#    designed to manage the core business of higher education institutions,
+#    such as universities, faculties, institutes and professional schools.
+#    The core business involves the administration of students, teachers,
+#    courses, programs and so on.
+#
+#    Copyright (C) 2015-2022 Université catholique de Louvain (http://www.uclouvain.be)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU General Public License for more details.
+#
+#    A copy of this license - GNU General Public License - is available
+#    at the root of the source code of this program.  If not,
+#    see http://www.gnu.org/licenses/.
+#
+##############################################################################
+import django.db
+from django.contrib.auth.models import User
+
+from osis_common.status.service_status import ServiceStatus, ServiceStatusError, ServiceStatusSuccess
+
+SERVICE_NAME = "database"
+
+
+def check_db() -> 'ServiceStatus':
+    """
+    Check that the database connection works.
+    :return ServiceStatus
+    """
+    try:
+        User.objects.all().first()
+        django.db.connection.ensure_connection()
+    except Exception as e:
+        return ServiceStatusError(service=SERVICE_NAME, original_error=e)
+    return ServiceStatusSuccess(service=SERVICE_NAME)
+
