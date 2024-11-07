49ba4d127b4b86be3371050476d830780f152c48
Julien Cougnaud <julien.cougnaud@makina-corpus.com>
2024-11-07 16:25:57 +0100

    [OS-1308] Fix the url fetcher in qa / prod

--- utils/url_fetcher.py
+++ utils/url_fetcher.py
@@ -28,7 +28,6 @@ from pathlib import Path
 from urllib.parse import urlparse
 
 from django.conf import settings
-from django.contrib.staticfiles.finders import find
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.core.files.storage import default_storage
 from django.urls import get_script_prefix
@@ -65,9 +64,7 @@ def django_url_fetcher(url):  # pragma: no cover
         elif settings.STATIC_URL and url_path.startswith(settings.STATIC_URL):
             path = url_path.replace(settings.STATIC_URL, '', 1)
             data['file_obj'] = staticfiles_storage.open(path)
-            absolute_path = find(path)
-            if absolute_path:
-                data['redirected_url'] = 'file://' + absolute_path
+            data['redirected_url'] = 'file://' + staticfiles_storage.path(path)
             return data
 
     # Fall back to weasyprint default fetcher
