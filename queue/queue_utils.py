77be9fb1b60f4c1e68f28eff77d6d907f161a6a2
Gaëtan La Marca <gaetan.lamarca@uclouvain.be>
2024-11-08 19:18:10 +0100

    Correction pylint

--- queue/queue_utils.py
+++ queue/queue_utils.py
@@ -9,9 +9,11 @@ if hasattr(settings, 'QUEUE_SSL_CONEXION') and settings.QUEUE_SSL_CONEXION:
 logger = logging.getLogger(settings.DEFAULT_LOGGER)
 queue_exception_logger = logging.getLogger(settings.QUEUE_EXCEPTION_LOGGER)
 
+
 def get_pika_connexion_parameters(queue_name='', client_properties: Optional[Dict] = None) -> pika.ConnectionParameters:
     if hasattr(settings, 'QUEUE_SSL_CONEXION') and settings.QUEUE_SSL_CONEXION:
-        logger.debug("Connecting to {0} (queue name = {1})... en SSL".format(settings.QUEUES.get('QUEUE_URL'), queue_name))
+        logger.debug("Connecting to {0} (queue name = {1})... en SSL".format(settings.QUEUES.get('QUEUE_URL'),
+                                                                             queue_name))
         context = ssl.create_default_context(cafile=settings.get('QUEUE_CLIENT_CA_CERT_PATH'))
         context.verify_mode = ssl.CERT_OPTIONAL
         context.load_cert_chain(certfile=settings.get('QUEUE_CLIENT_CERT_PATH'),
