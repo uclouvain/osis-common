32997fdc1419a6c16eb6703fe99be0bd73b5876c
Gaëtan La Marca <gaetan.lamarca@uclouvain.be>
2024-11-09 15:52:09 +0100

    Correction de l'accès aux certificat pika

--- queue/queue_utils.py
+++ queue/queue_utils.py
@@ -14,10 +14,10 @@ def get_pika_connexion_parameters(queue_name='', client_properties: Optional[Dic
     if hasattr(settings, 'QUEUE_SSL_CONEXION') and settings.QUEUE_SSL_CONEXION:
         logger.debug("Connecting to {0} (queue name = {1})... en SSL".format(settings.QUEUES.get('QUEUE_URL'),
                                                                              queue_name))
-        context = ssl.create_default_context(cafile=settings.get('QUEUE_CLIENT_CA_CERT_PATH'))
+        context = ssl.create_default_context(cafile=settings.QUEUE_CLIENT_CA_CERT_PATH)
         context.verify_mode = ssl.CERT_OPTIONAL
-        context.load_cert_chain(certfile=settings.get('QUEUE_CLIENT_CERT_PATH'),
-                                keyfile=settings.get('QUEUE_CLIENT_CERT_KEY_PATH'))
+        context.load_cert_chain(certfile=settings.QUEUE_CLIENT_CERT_PATH,
+                                keyfile=settings.QUEUE_CLIENT_CERT_KEY_PATH)
         ssl_options = pika.SSLOptions(context=context,
                                       server_hostname=settings.QUEUES.get('QUEUE_URL'))
         credentials = pika.PlainCredentials(username=settings.QUEUES.get('QUEUE_USER'),
